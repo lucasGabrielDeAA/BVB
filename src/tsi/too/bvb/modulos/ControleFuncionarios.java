f7ea132454a835a61cc2780974b4d5b67f39e416
higor-bianchetti <higorbianchetti@gmail.com>
2014-06-20 21:38:17 -0300

    Pacote Modulos -> Classe ControleFuncionarios

---
+++ src/tsi/too/bvb/modulos/ControleFuncionarios.java
@@ -0,0 +1,21 @@
+package br.com.bvb.modulos;
+
+public class ControleFuncionarios {
+
+	private final int MAX_CLIENTES = 10; 
+	private Cliente clientes[];
+	private int numeroClientes;
+	
+	public ControleFuncionarios() {
+		clientes = new Cliente[MAX_CLIENTES];
+		numeroClientes = 0;
+	}
+	
+	public boolean cadastrarCliente(Cliente pessoa) {
+		if(numeroClientes >= MAX_CLIENTES)
+			return false;
+		clientes[numeroClientes] = pessoa;
+		return true;
+	}
+	
+} //class ControleFuncionarios
