71cdf84c9983f817ed91df62b16c0924f3872d47
Lucas <lg.technus@gmail.com>
2014-06-26 21:11:18 -0300

    Alteração no tempo de execução da tela de Splah

--- src/tsi/too/bvb/GUI/StartSplash.java
+++ src/tsi/too/bvb/GUI/StartSplash.java
@@ -78,7 +78,7 @@ public class StartSplash extends JFrame
   {
    try
    {
-	Thread.sleep(barra*100);
+	Thread.sleep(barra*30);
 	
 	barraDeProgresso.setValue(barra);
 	etapasLabel.setText(etapas[barra]);
