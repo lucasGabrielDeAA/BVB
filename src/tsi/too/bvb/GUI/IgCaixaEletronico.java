b89e0fe4fdc8254c9acb86f4a0269530344a715c
Lucas <lg.technus@gmail.com>
2014-07-05 16:09:25 -0300

    Alteração na classe IgCaixaEletronico

--- src/tsi/too/bvb/GUI/IgCaixaEletronico.java
+++ src/tsi/too/bvb/GUI/IgCaixaEletronico.java
@@ -6,16 +6,20 @@ import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.ImageIcon;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class IgCaixaEletronico extends JFrame
 {//Início do corpo da classe.
 	
+ private Font fonte = new Font("Nimbus Sans L", Font.BOLD, 20); 
+	
  public IgCaixaEletronico()
  {
- 	setFont(new Font("Nimbus Sans L", Font.BOLD, 12));
+  setFont(fonte);
   setTitle("Caixa Eletrônico");
-  getContentPane().setFont(new Font("Nimbus Sans L", Font.BOLD, 12));
+  getContentPane().setFont(fonte);
   getContentPane().setLayout(null);
   
   JLabel wallPaperLabel = new JLabel("");
@@ -24,35 +28,51 @@ public class IgCaixaEletronico extends JFrame
   getContentPane().add(wallPaperLabel);
   
   JButton saqueButton = new JButton("SAQUE");
-  saqueButton.setFont(new Font("Nimbus Sans L", Font.BOLD, 20));
+  saqueButton.addActionListener(new ActionListener()
+  {
+   public void actionPerformed(ActionEvent arg0)
+   {
+	try
+	{
+	 IgSaque saque = new IgSaque();
+	}catch (Exception e)
+	{
+ 	 e.printStackTrace();
+	}
+   }
+  });
+  saqueButton.setFont(fonte);
   saqueButton.setBounds(73, 147, 190, 64);
   wallPaperLabel.add(saqueButton);
   
   JButton depositoButton = new JButton("DEPÓSITO");
-  depositoButton.setFont(new Font("Nimbus Sans L", Font.BOLD, 20));
+  depositoButton.setFont(fonte);
   depositoButton.setBounds(73, 223, 190, 64);
   wallPaperLabel.add(depositoButton);
   
   JButton saldoButton = new JButton("SALDO");
-  saldoButton.setFont(new Font("Nimbus Sans L", Font.BOLD, 20));
+  saldoButton.setFont(fonte);
   saldoButton.setBounds(73, 299, 190, 64);
   wallPaperLabel.add(saldoButton);
   
   JButton extratoSimplesButton = new JButton("EXTRATO");
-  extratoSimplesButton.setFont(new Font("Nimbus Sans L", Font.BOLD, 20));
+  extratoSimplesButton.setFont(fonte);
   extratoSimplesButton.setBounds(533, 147, 190, 64);
   wallPaperLabel.add(extratoSimplesButton);
   
   JButton tranfButton = new JButton("TRANFERÊNCIA");
-  tranfButton.setFont(new Font("Nimbus Sans L", Font.BOLD, 20));
+  tranfButton.setFont(fonte);
   tranfButton.setBounds(533, 223, 190, 64);
   wallPaperLabel.add(tranfButton);
   
   JButton resgateButton = new JButton("RESGATE");
-  resgateButton.setFont(new Font("Nimbus Sans L", Font.BOLD, 20));
+  resgateButton.setFont(fonte);
   resgateButton.setBounds(533, 299, 190, 64);
   wallPaperLabel.add(resgateButton);
   
   setSize(800, 600);
+  setLocationRelativeTo(null);
+  setResizable(false);
+  setVisible(true);
  }
 }//Fim do corpo da classe.
